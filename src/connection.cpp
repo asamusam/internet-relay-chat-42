fd45adc491a2076224674db1f5357d19872b675e
agarraga <83713047+agarraga@users.noreply.github.com>
2024-10-24 20:41:39 +0100

    fix client error
    the accept_in_conn() function, responsible for creating new Client,
    wasn't assigning uuid to said clients and they were being overwritten

--- src/connection.cpp
+++ src/connection.cpp
@@ -80,7 +80,9 @@ void accept_in_conns(App &app, int epoll_fd, int listen_sock_fd)
 	if (-1 == epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn_sock_fd, &ev))
 		throw (SCEM_EPOLL_CTL);
 
+	static int uuid = 1;
 	Client *client = new Client;
+	client->uuid = uuid++;
 	client->fd = conn_sock_fd;
 	client->is_registered = false;
 	client->has_valid_pwd = false;
